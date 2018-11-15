f71dd1d5ea139c2e74a22c3d047dab8dcf59b926
katejsenger <katejsenger@yahoo.com>
2018-11-15 19:57:47 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
     @brand = brand
   end
   def cobble(shoe)
-    if shoe.condition = new
+    if condition = new
     puts "Your shoe is as good as new!"
   else  
     puts "Your show has been repaired!"
